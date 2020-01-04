41464853617449e1800286baa5502fc7ac2c3c97
Sunil Ngl <Sunil Ngl@192.168.1.5>
2020-01-04 18:39:06 +0000

    merged

--- One/demo/src/main/java/com/app/demo/DemoApplication.java
+++ One/demo/src/main/java/com/app/demo/DemoApplication.java
@@ -9,6 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	public String getChild() {
+		return "sample String";
+	}
 	public String getParent() {
 		return "sample String";
 	}
